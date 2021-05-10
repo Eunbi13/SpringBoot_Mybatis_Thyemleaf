81d4986cfe0524a219f1afa9775a099631b42e0d
ba-lim <dmsqltkfk276@naver.com>
2021-05-10 16:41:01 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBootMybatisThyemleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisThyemleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisThyemleafApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+/**
+ * =====================
+	Thymeleaf 프로젝트입니다
+ * =====================
+*/
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("message", "Thymeleaf project");
+		return "index";
+	}
+}
